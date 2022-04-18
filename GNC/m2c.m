62d18356ca45db6447bc4156e4fc5c44767285b1
Chihaya Kawamura <ckck4253@gmail.com>
2022-04-18 20:02:52 +0900

    Update m2c.m

--- GNC/m2c.m
+++ GNC/m2c.m
@@ -34,13 +34,13 @@ if (length(nu) == 6)     % 6-DOF model
     
     M11 = M(1:3,1:3);
     M12 = M(1:3,4:6);
-    M21 = M12.';
+    M21 = M12';
     M22 = M(4:6,4:6);
     
     nu1 = nu(1:3);
     nu2 = nu(4:6);
-    dt_dnu1 = M11*nu1.' + M12*nu2.';
-    dt_dnu2 = M21*nu1.' + M22*nu2.';
+    dt_dnu1 = M11*nu1' + M12*nu2';
+    dt_dnu2 = M21*nu1' + M22*nu2';
     
     C = [  zeros(3,3)      -Smtrx(dt_dnu1)
         -Smtrx(dt_dnu1)  -Smtrx(dt_dnu2) ];
